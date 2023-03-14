a0d3703e517f564e36be83e7221942b5cc3a5b60
prgangrade <prgangrade@gmail.com>
2023-03-14 17:16:14 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   has_many :frieends
 
